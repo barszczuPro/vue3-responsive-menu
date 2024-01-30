249734a13999dbda4ecd5eecd9af061b2314b810
Rafał Barszczowski <barszczu_pro@barszczu-pro.local>
2024-01-30 10:45:15 +0100

    Gh actions - deploy demo

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -4,4 +4,16 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/vue3-responsive-menu/',
+  resolve: {
+    alias: {
+      vue: 'vue/dist/vue.esm-bundler.js',
+    },
+    dedupe: ['vue'],
+  },
+  server: {
+    fs: {
+      allow: ['..'],
+    },
+  },
 })
