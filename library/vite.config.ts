7b191b5a74738095d4473ae65c84e9fa91fabac3
barszczuPro <barszczowski.rafal@gmail.com>
2024-02-05 22:58:32 +0100

    Add config lib build

--- library/vite.config.ts
+++ library/vite.config.ts
@@ -5,6 +5,24 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    lib: {
+      entry: 'index.ts',
+      name: 'vue3-responsive-menu',
+      fileName: (format) => `vue3-responsive-menu.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue'
+        },
+        assetFileNames: 'vue3-responsive-menu.[ext]',
+      },
+    },
+    sourcemap: true,
+    outDir: '../lib',
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./', import.meta.url))
